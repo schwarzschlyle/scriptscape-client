ef2b0aca31c04db86ea3e087af0547bd5a9a1c24
lyle-offshorly <lyle@offshorly.com>
2025-12-27 02:07:40 +0800

    feat: added credentials sending session tokens to all api requests

--- src/api/client.ts
+++ src/api/client.ts
@@ -7,6 +7,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 
 import type { InternalAxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
